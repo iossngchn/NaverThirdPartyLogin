b32d465177176afd98c861ce7a3dc591aba916c4
iossngchn <iosdevsngchn@gmail.com>
2024-03-14 14:48:32 +0900

    initial commit

---
+++ NaverThirdPartyLogin/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NaverThirdPartyLogin",
+    products: [
+        .library(
+            name: "NaverThirdPartyLogin",
+            targets: [
+                "NaverThirdPartyLogin",
+            ]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "NaverThirdPartyLogin",
+            path: "Sources/NaverThirdPartyLogin/NaverThirdPartyLogin.xcframework"
+        ),
+    ]
+)
